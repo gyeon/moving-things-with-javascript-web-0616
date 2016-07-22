33b37d3cd58003b8032393a5a2b69094f1406adb
gyeon <gyeon19@gmail.com>
2016-07-22 09:55:43 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-const app = "I don't do much."
+// const app = "I don't do much."
+// document.addEventListener('keydown', function(e) {
+//   console.log(e.which)
+// });
+// // right arrow === 39
+document.addEventListener('keydown', function(e) {
+  if (e.which === 39) {
+    var rightNumbers =
+    dodger.style.right.replace('px', '')
+    var right = parseInt(rightNumbers, 10)
+    dodger.style.right = `${right -1}px`
+  }
+});
+
+var dodger = document.getElementById('dodger')
+
+function moveDodgerRight() {
+  var rightNumbers =
+  dodger.style.right.replace('px', '')
+  var right = parseInt(rightNumbers, 10)
+  if (right > 0) {
+    dodger.style.right = `${right -1}px`
+  }
+}
